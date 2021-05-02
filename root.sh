dbd299cdce9e4108e5ee092777dd8691a25c6c35
centsmith001 <79389709+centsmith001@users.noreply.github.com>
2021-05-02 15:11:55 +0800

    Update root.sh

--- root.sh
+++ root.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-sudo su -
 sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/g' /etc/ssh/sshd_config
 sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config
-systemctl resart ssh
-clear
+clear                                                                                    
 echo -e 'To set password just run "passwdroot" command'
-echo -e 'Go to your VPS credentials or "identities" if you are using juicessh'
-echo -e 'Set your username to "root" then input your password"
-
+echo -e 'Go to your VPS credentials or "identities" if you are using juicessh'          
+echo -e 'Set your username to "root" then input your password"'
+systemctl restart ssh
+exit 1
